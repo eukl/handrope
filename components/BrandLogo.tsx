0ea5d1680ffb703108272c5d8a0e291eabc613a4
Louis Kessler <louis.kessler@outlook.fr>
2026-06-24 19:31:55 +0200

    Feat: Test New Logo 2

--- components/BrandLogo.tsx
+++ components/BrandLogo.tsx
@@ -27,7 +27,7 @@ export default function BrandLogo({
         height={518}
         priority={!showParis}
         sizes="(max-width: 640px) 150px, 170px"
-        className="h-[1.75em] w-auto select-none object-contain opacity-[0.96]"
+        className="h-[1.75em] w-auto select-none object-contain"
       />
       <span className="sr-only">{label}</span>
     </span>
